5b2fb1931911a8816293ba1da43b1a28c2cffe35
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2024-02-10 13:55:54 -0500

    .gitignore updates

--- server/cloudStorage.js
+++
@@ -1,8 +0,0 @@
-const { Storage } = require("@google-cloud/storage");
-const storage = new Storage({
-  projectId: "ugos-portfolio",
-  keyFilename: "ugos-portfolio-db71ff131f10.json",
-});
-
-const bucket = storage.bucket("ugo-portfolio-media");
-module.exports = bucket;
